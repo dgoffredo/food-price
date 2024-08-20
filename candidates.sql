3f7e1728141a1eddfd8a0b7a4eb9ba707bc51677
David Goffredo <dmgoffredo@gmail.com>
2024-08-20 16:00:17 -0400

    TODO: AMEND: this might work

--- candidates.sql
+++ candidates.sql
@@ -4,7 +4,7 @@
 -- If a store has had its catalog scraped before, then the stores for which
 -- it's been the longest take priority.
 -- The query returns all columns for the most recent ScrapedStore having the
--- relevant store code.
+-- corresponding store code.
 with CandidateStore as (
   select store2.code, store2.id, max(session.when_begin_iso) as last_scraped
   from ScrapedStore store left join ScrapeSession session
@@ -12,7 +12,9 @@ with CandidateStore as (
   inner join ScrapedStore store2 on store2.code  = store.code
   group by store.code, store.when_iso
   having store2.when_iso = max(store.when_iso))
-select store.*
+select store.id, store.code, url.domain, url.slug
 from ScrapedStore store inner join CandidateStore candidate
   on store.id = candidate.id
+inner join StoreURL url
+  on url.id = store.url
 order by candidate.last_scraped, candidate.code;
